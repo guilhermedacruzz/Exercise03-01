1b3667249e6bbb9a5411839cc94c4463db7a2d06
Guilherme da Cruz <gddcruz04@gmail.com>
2020-03-06 14:56:49 -0300

    Consertando erro apontado pelo professor

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,75 +4,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    private String nome;
-    private int fichas;
-    private int fichasGanhas;
-    private int fichasPerdidas;
-    private int rodadas;
+    public String nome;
+    public  int fichas;
+    public int fichasGanhas;
+    public int fichasPerdidas;
+    public int rodadas;
 
     public Game(String nome, int fichas) {
         this.nome = nome;
         this.fichas = fichas;
     }
 
-    public void fazerAposta() {
+    public int fazerAposta(int aposta, int palpite) {
         int dado1 = new Random().nextInt(6) + 1;
         int dado2 = new Random().nextInt(6) + 1;
-        int aposta, palpite;
-
-        //System.out.println("Resultado da Soma dos Dados:    " + (dado1 + dado2));
-        System.out.printf("\nDeseja apostar quantas fichas ?\t");
-        do{
-            aposta = new Scanner(System.in).nextInt();
-        }while(aposta < 0 || aposta > this.fichas);
-
-        System.out.printf("\nPalpite do resulatdo: \t");
-        do{
-            palpite = new Scanner(System.in).nextInt();
-        }while(palpite < 1);
-
 
         if(palpite == dado1 + dado2){
-            System.out.println("Você ganhou a rodada :( +" + (aposta * 2) + " fichas!");
             this.fichas += aposta * 2;
             this.fichasGanhas += 1;
+            aposta *= 2;
         }
         else if(Math.abs(palpite - dado1 + dado2) == 1) {
-            System.out.println("Você quase ganhou a rodada :( +" + (aposta / 2) + " fichas!");
             this.fichas += aposta / 2;
             this.fichasGanhas  += 1;
+            aposta /= 2;
         }
         else{
-            System.out.println("Você perdeu a rodada :( -" + aposta + " fichas!");
             this.fichas -= aposta;
             this.fichasPerdidas += 1;
+            aposta *= -1;
         }
 
         this.rodadas += 1;
-        System.out.println();
-        System.out.println("Resultado da Rodada" + this.rodadas + " é " + dado1 + " " + dado2);
+        return aposta;
     }
 
-    public void status()  {
-        System.out.printf("\n\nRelatório\n\n");
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Fichas restantes: " + this.fichas);
-        System.out.println("Rodadas Jogadas: " + this.rodadas);
-        System.out.println("Fichas Ganhas: " + this.fichasGanhas);
-        System.out.println("Fichas Perdidas: " + this.fichasPerdidas);
+    @Override
+    public String toString() {
+        return "Game{" +
+                "nome='" + nome + '\'' +
+                ", fichas=" + fichas +
+                ", fichasGanhas=" + fichasGanhas +
+                ", fichasPerdidas=" + fichasPerdidas +
+                ", rodadas=" + rodadas +
+                '}';
     }
-
-    public boolean verificarContinuidade() {
-        if(fichas == 0)
-            return false;
-
-        System.out.println("Deseja continuar jogando? (S/N)");
-        char escolha = new Scanner(System.in).next().charAt(0);
-
-        if(Character.toLowerCase(escolha) == 'n')
-            return false;
-
-        return true;
-    }
-
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         String nome;
-        int fichas;
+        int fichas, aposta, palpite;
+        boolean isRunning = true;
         Scanner sc = new Scanner(System.in);
 
         System.out.printf("\nCadastro\n\n");
@@ -21,10 +22,29 @@ public class Main {
         Game jogo = new Game(nome, fichas);
 
         do{
+            System.out.printf("\nDeseja apostar quantas fichas ?\t");
+            do{
+                aposta = sc.nextInt();
+            }while(aposta <= 0 || aposta > jogo.fichas);
 
-            jogo.fazerAposta();
+            System.out.printf("\nPalpite do resulatado: \t");
+            do{
+                palpite = new Scanner(System.in).nextInt();
+            }while(palpite < 1);
 
-        }while(jogo.verificarContinuidade());
-        jogo.status();
+            System.out.printf("Resultado da rodada:\t%d fichas\n", jogo.fazerAposta(aposta, palpite));
+
+            if(jogo.fichas == 0){
+                break;
+            }
+
+            System.out.println("Deseja continuar jogando? (S/N)");
+            char escolha = sc.next().charAt(0);
+
+            if(Character.toLowerCase(escolha) == 'n')
+                isRunning = false;
+        }while(isRunning);
+
+        System.out.println(jogo.toString());
     }
 }
